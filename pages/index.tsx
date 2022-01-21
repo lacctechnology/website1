f747655d1716fe258bf140ae99843b1a404ae6a7
Cesar Puentes <caps2695@gmail.com>
2022-01-21 14:40:45 -0500

    🚧 🔥 🎨 res estructuracion a typescrit y arreglo a la pagina Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,41 @@
-import React from 'react';
+import type { NextPage } from 'next'
+import Image from 'next/image'
 
-const HomePage = () => {
+const HomePage: NextPage = () => {
   return (
-    <section className='main'>
-      <div className='container'>
+    <div className='container'>
+      <section className='main'>
         <div className='main-container'>
           <h1>
             <span>En</span> construcción
           </h1>
+          <p>Revisa nuestras redes sociales</p>
           <ul className='main-redes'>
             <li>
               <a href='https://www.facebook.com/Lacc-Technology-335501074366818'>
-                <img
-                  className='facebook'
+                <Image
                   src='/images/facebook.png'
                   alt='facebook'
+                  width={400}
+                  height={400}
                 />
               </a>
             </li>
             <li>
               <a href='https://www.instagram.com/lacc_technology/'>
-                <img
-                  className='instagram'
+                <Image
                   src='/images/instagram.png'
                   alt='instagram'
+                  width={400}
+                  height={400}
                 />
               </a>
             </li>
           </ul>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    </div>
+  )
+}
 
-export default HomePage;
+export default HomePage
