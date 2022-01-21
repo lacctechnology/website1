f73658f7ab62a40e8a40505f988a0efdbb73d91a
Cesar Puentes <caps2695@gmail.com>
2022-01-21 17:45:22 -0500

    🐛 arreglo del titulo de la pagina

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import '../styles/normalize.css'
 import '../styles/style.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Lacc Technology</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
